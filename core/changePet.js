4e0be6b32a8196a49c9320d53765204f5a8a1471
圆周率也想学live2d <gcb384076498@126.com>
2024-04-28 23:56:56 +0800

    更新逻辑

--- core/changePet.js
+++ core/changePet.js
@@ -1,45 +1,50 @@
-(function() {
-    var BASE_URL = 'https://gcb384076498.github.io/SwyAssets'
+var BASE_URL = 'https://gcb384076498.github.io/SwyAssets'
+var selector = document.getElementById("pets")
+var changePet = document.getElementById("changePet")
 
 
 
-    function listModel() {
-        return new Promise(function(resolve, reject) {
-            fetch(BASE_URL+"/index.json", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(function(response) {
-                return response.text()
-            }).then(function(data) {
-                var dataJson = JSON.parse(data)
-                resolve(dataJson)
-            }).catch(function (error) {
-                reject(error)
-            })
+function listModel() {
+    return new Promise(function(resolve, reject) {
+        fetch(BASE_URL+"/index.json", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(function(response) {
+            return response.text()
+        }).then(function(data) {
+            var dataJson = JSON.parse(data)
+            resolve(dataJson)
+        }).catch(function (error) {
+            reject(error)
         })
-    }
+    })
+}
 
-    function  createElement(tag, attrs, innerText) {
-        var element = document.createElement(tag)
-        for (var attr in attrs) {
-            element.setAttribute(attr, attrs[attr])
-        }
-        element.innerText = innerText
-        return element
+function  createElement(tag, attrs, innerText) {
+    var element = document.createElement(tag)
+    for (var attr in attrs) {
+        element.setAttribute(attr, attrs[attr])
     }
+    element.innerText = innerText
+    return element
+}
 
 
-    function saveCurrentPet(config, modelName) {
-        var configObj = {
-            type: config["model_type"] === 1 ? "spine" : "live2d",
-            modelId: modelName
-        }
-        localStorage.setItem('lastModel', JSON.stringify(configObj))
-        console.log("保存成功", configObj)
-        alert("保存成功")
+function saveCurrentPet(config, modelName) {
+    var configObj = {
+        type: config["model_type"] === 1 ? "spine" : "live2d",
+        modelId: modelName
     }
+    localStorage.setItem('lastModel', JSON.stringify(configObj))
+    console.log("保存成功", configObj)
+    alert("保存成功")
+}
+
+
+
+(function() {
 
     if (!localStorage.getItem('test')) {
         console.log("非测试模式233")
@@ -65,21 +70,23 @@
                 })
             }, data[pets[i]]["name"]))
         }
-        var selector = document.getElementById("pets")
-        selector.innerHTML = p.innerHTML
-        selector.addEventListener('change', function (e) {
-            var modelName = e.target.value
+        changePet.onclick = function() {
+            var modelName = selector.value
             console.log("选择的模型是", modelName)
             if (modelName === '--请选择--') {
                 return
             }
             if (data[modelName]) {
                 saveCurrentPet(data[modelName], modelName)
-                // window.location.reload()
+                window.location.reload()
             }
-        })
+
+        }
+        selector.innerHTML = p.innerHTML
         // 加载场景
     }).catch(function (error) {
         console.log("加载配置失败", error)
     })
+
+
 })()
